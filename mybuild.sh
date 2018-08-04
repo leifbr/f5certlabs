7d2e599d5d967b76ed1df6da9cb8a53d6b82b3e6
Leif Rasmussen <l.rasmussen@f5.com>
2018-08-04 16:53:45 -0700

    updates

---
+++ mybuild.sh
@@ -0,0 +1,1 @@
+exec docker run --rm -it -v /home/f5student/Dropbox/github/f5certlabs:/home/f5student/Dropbox/github/f5certlabs --workdir /home/f5student/Dropbox/github/f5certlabs -e LOCAL_USER_ID=1002 0xhiteshpatel/containthedocs:2.3 make -C docs html
